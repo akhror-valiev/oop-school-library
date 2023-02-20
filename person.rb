b9bad8841221809146a26626e562dbfaef38565f
akhror-valiev <ahror.valiev@gmail.com>
2023-02-20 19:03:07 +0500

    Add Classes

--- person.rb
+++ person.rb
@@ -1,24 +1,22 @@
-class  Person
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
 
-    attr_accessor :name :age
-    attr_reader :id
 
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def initialize(name="Unknown", age, parent_permission=true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  def can_use_services?
+    true if of_age? || @parent_permission
+  end
 
-    def can_use_services?
-       true if  is_of_age?  || @parent_permission
-    end
+  private
 
-    private
-    def is_of_age?
-    true if age>=18
-
-    end
-
+  def of_age?
+    true if age >= 18
+  end
 end
