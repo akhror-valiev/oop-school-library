9e159eab414197f884ec0a3ed87217c80a25def1
akhror-valiev <ahror.valiev@gmail.com>
2023-02-22 08:32:04 +0500

    Update decorators

--- nameable.rb
+++ nameable.rb
@@ -1,6 +1,5 @@
 class Nameable
-  def correct_name(name)
-    name.capitalize!
-    name[0, 10]
+  def correct_name
+    raise NotImplementedError
   end
 end
--- person.rb
+++ person.rb
@@ -1,23 +1,26 @@
 require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
 
-class Person
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @nameable = Nameable.new
   end
 
   def can_use_services?
     true if of_age? || @parent_permission
   end
 
-  def valid_name
-    @name = @nameable.correct_name(@name)
+  def correct_name
+    @name
   end
 
   private
@@ -26,5 +29,10 @@ class Person
     true if age >= 18
   end
 end
+
 person = Person.new(22, 'maximilianus')
-puts person.valid_name
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
