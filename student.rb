7fab50a156990345534fd066c24b242683b6483a
akhror-valiev <ahror.valiev@gmail.com>
2023-02-24 07:22:27 +0500

    Class association

--- student.rb
+++ student.rb
@@ -9,4 +9,10 @@ class Student < Person
   def play_hooky()
     '¯\(ツ)/¯'
   end
+  attr_accessor :classroom
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.students.include?(self)
+  end
 end
