704d46b460f04c35d4aad5dd6aa188d89999f3ae
akhror-valiev <ahror.valiev@gmail.com>
2023-02-25 13:12:52 +0500

    Add basic yi

---
+++ app.rb
@@ -0,0 +1,116 @@
+require './book'
+require './rental'
+require './student'
+require './teacher'
+
+
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rental = []
+  end
+
+  def books_list
+    if @books.empty?
+      puts 'Books list is empty try add a book!'
+    else
+      @books.each_with_index do |book, index|
+        puts "(#{index}) Title: #{book.title}, Author: #{book.author}"
+      end
+    end
+  end
+
+  def people_list
+    if @people.empty?
+      puts 'People list is empty try add a people!'
+    else
+      @people.each do |person|
+        puts "[#{person.class}] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+      end
+    end
+  end
+
+  def show_people
+    @people.each_with_index do |person, index|
+      puts "(#{index}) [#{person.class}] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+    end
+  end
+
+  def show_rental
+    puts 'Select person'
+    show_people
+    print 'Enter selected person ID: '
+    id = gets.chomp.to_i
+    puts 'Rentals:'
+    @rental.each do |rent|
+      puts " Date: #{rent.date} Book: #{rent.book.title} Author: #{rent.book.author}" if rent.person.id == id
+    end
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+    num = gets.chomp
+    case num
+    when '1'
+      print 'Age:'
+      age = gets.chomp
+      print 'Name:'
+      name = gets.chomp
+      print 'Parent permisssion [y/n]:'
+      permission = gets.chomp
+      print 'Enter classroom: '
+      classroom = gets.chomp
+      student = Student.new(classroom, age, name, parent_permission: permission)
+      @people.push(student)
+      puts 'Person created successfully!'
+
+    when '2'
+      print 'Age:'
+      age = gets.chomp
+      print 'Name:'
+      name = gets.chomp
+      print 'Specialization:'
+      specialization = gets.chomp
+      teacher = Teacher.new(specialization, age, name, parent_permission: true)
+      @people.push(teacher)
+      puts 'Person created successfully!'
+
+    else
+      puts 'invalid'
+    end
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    @books.push(book)
+    puts 'Book created successfully!'
+  end
+
+  def create_rental
+    if @books.empty?
+      puts 'Book list is empty please create a book first'
+    else
+      puts 'Select a book from the following list by number'
+      books_list
+      print '\nEnter book number: '
+      book_id = gets.chomp.to_i
+
+      puts 'Select a person from the following list by number (not id)'
+      show_people
+      print '\nEnter person number: '
+      person_id = gets.chomp.to_i
+
+      print '\nDate: '
+      date = gets.chomp
+
+      rent = Rental.new(date, @people[person_id], @books[book_id])
+      @rental.push(rent)
+      puts 'Rental created successfully!'
+    end
+  end
+end
---
+++ main.rb
@@ -0,0 +1,42 @@
+require './app'
+
+class Main
+  app = App.new
+
+  puts 'welcome to School Library App!'
+  puts 'Please choose an option by entering a number:'
+
+  loop do
+    puts "\n"
+    puts '1 - List all of the books'
+    puts '2 - List all of the people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all of the rentals for the given person id'
+    puts '7 - Exit'
+    puts "\n"
+
+    option = gets.chomp
+
+    case option
+    when '1'
+      app.books_list
+    when '2'
+      app.people_list
+    when '3'
+      app.create_person
+    when '4'
+      app.create_book
+    when '5'
+      app.create_rental
+    when '6'
+      app.show_rental
+    when '7'
+      puts puts "Thank you for using this app!\n\n"
+      break
+    else
+      puts 'Incorrect choice, Please choose between 1 and 7'
+    end
+  end
+end
